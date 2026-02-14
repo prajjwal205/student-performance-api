70afe86bc6a04270b52b1bce7443806dfc239bc0
prajjwal205 <prajjwalsrivastav2050@gmail.com>
2026-02-14 21:18:38 +0530

    Student performance prediction API

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+model = joblib.load("student_model.pkl")
+columns = joblib.load("model_columns.pkl")
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Student Performance Prediction API is running!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    df_input = pd.DataFrame([data])
+
+    df_encoded = pd.get_dummies(df_input, drop_first=True)
+    df_encoded = df_encoded.reindex(columns=columns, fill_value=0)
+
+    prediction = model.predict(df_encoded)[0]
+
+    return jsonify({"predicted_percentage": float(prediction)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
