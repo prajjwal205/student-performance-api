863161cf617f5547677d109c70328ef0f957e0b4
prajjwal205 <prajjwalsrivastav2050@gmail.com>
2026-02-14 22:46:56 +0530

    Fix render port issue

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -25,7 +26,6 @@ def predict():
     prediction = model.predict(df_encoded)[0]
 
     return jsonify({"predicted_percentage": float(prediction)})
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
